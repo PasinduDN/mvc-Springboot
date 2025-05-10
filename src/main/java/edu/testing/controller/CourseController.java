7a7b6546e9f3668a0ac0230ee9bea6d105829d58
Pasindu Dulanjana <pasindu@agrithmics.lk>
2025-05-10 22:14:17 +0530

    Layers Decoupling

--- src/main/java/edu/testing/controller/CourseController.java
+++ src/main/java/edu/testing/controller/CourseController.java
@@ -2,19 +2,21 @@ package edu.testing.controller;
 
 import edu.testing.dto.Course;
 import edu.testing.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CourseController {
 
+    @Autowired
+    CourseService courseService;
+
     @GetMapping("/getListOfCourseList")
     public List<Course> getListOfCourse(){
-        CourseService courseService = new CourseService();
-        List<Course> courses = courseService.getListOfCourse();
-        return  courses;
+
+        return courseService.getListOfCourse();
     }
 }
--- src/main/java/edu/testing/service/impl/CourseServiceImpl.java
+++ src/main/java/edu/testing/service/impl/CourseServiceImpl.java
@@ -1,11 +1,13 @@
-package edu.testing.service;
+package edu.testing.service.impl;
 
 import edu.testing.dto.Course;
-
+import edu.testing.service.CourseService;
+import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CourseService {
+@Service
+public class CourseServiceImpl implements CourseService {
     public List<Course> getListOfCourse(){
         ArrayList<Course> coursesList = new ArrayList();
         coursesList.add(new Course("D001","001", "50000","15"));
